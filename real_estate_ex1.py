87b5420fd8cbd3269e8d07a0f0bc272bbc63a7cf
yongmin.ko <euclidmin@gmail.com>
2018-10-13 14:27:14 +0900

    실거래 공공API 구현 및 xml parse 행안부 법정동 코드 parse

--- real_estate_ex1.py
+++ real_estate_ex1.py
@@ -1,6 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+import numpy as np
 import pprint
 from google.cloud import translate
 
@@ -15,12 +16,19 @@ def get_worksheet_client():
 
 
 # noinspection SpellCheckingInspection
-def get_sheet(client):
-    wsheet = client.open('소형아파트투자')
-    sheet = wsheet.worksheet('보유현황')
-    # wsheet = client.open("")
-    # sheet = wsheet.worksheet('보유현황')
+def get_sheet(client, wsheet_name=None, sheet_name=None):
+    if wsheet_name==None :
+        wsheet = client.open('소형아파트투자')
+    else :
+        wsheet = client.open(wsheet_name)
+
+    if sheet_name==None :
+        sheet = wsheet.worksheet('보유현황')
+    else :
+        sheet = wsheet.worksheet(sheet_name)
     return sheet
+
+
 
 
 def get_col_data(sheet, index):
@@ -28,12 +36,75 @@ def get_col_data(sheet, index):
     return col
 
 
+def extract_data_from_sheet(sheet):
+    local_code = get_col_data(sheet, 1)
+    city_state = get_col_data(sheet, 2)
+    city_gun_gu = get_col_data(sheet, 3)
+    eup_myen_dong = get_col_data(sheet, 4)
+
+    local_code_record = []
+    for a, b, c, d in zip(local_code, city_state, city_gun_gu, eup_myen_dong):
+        abcd = [a, b, c, d]
+        local_code_record.append(abcd)
+    return local_code_record
+
+def extract_data_from_sheet1(sheet):
+    local_code = get_col_data(sheet, 1)
+    city_state = get_col_data(sheet, 2)
+    city_gun_gu = get_col_data(sheet, 3)
+    eup_myen_dong = get_col_data(sheet, 4)
+
+    local_code_record = np.array([local_code, city_state, city_gun_gu, eup_myen_dong]).transpose()
+
+    print(local_code_record)
+    
+    return local_code_record
+
+def load_local_code_data(sheet):
+    import pickle
+    dataset_dir = os.path.dirname(os.path.abspath(__file__))
+    save_file = dataset_dir + "/localcode.pkl"
+
+    if not os.path.exists(save_file):
+        local_code_dataset = extract_data_from_sheet1(sheet)
+        with open(save_file, 'wb') as f:
+            pickle.dump(local_code_dataset, f)
+            print('save pkl file.')
+    with open(save_file, 'rb') as f:
+        dataset = pickle.load(f)
+        print('load pkl')
+
+    return dataset
+
+
+
+
+
+def make_local_city(lc_records):
+    ret_ = []
+    for lc_record in lc_records :
+        if lc_record[2]!='' and lc_record[3]=='' :
+            ret_.append([lc_record[0], lc_record[2]])
+
+    for pair in ret_ :
+        pair[0] = pair[0][0:5]
+
+    return ret_
+
+
 def main():
     ws_client = get_worksheet_client()
-    sh = get_sheet(ws_client)
+    # sh = get_sheet(ws_client)
+    sh = get_sheet(ws_client, 'KIKcd_H.20180122', 'KIKcd_H')
+    local_code_record = load_local_code_data(sh)
+    print(local_code_record)
+    localcode_cityname_ = make_local_city(local_code_record)
 
-    col_A = get_col_data(sh, 1)
-    print(col_A)
+    print(localcode_cityname_)
+
+
+
+
 
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -1,29 +1,58 @@
 import httplib2
 from urllib.parse import urlencode, unquote, quote_plus
 
-# http = httplib2.Http()
-# url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?"
-#
-# data = {'serviceKey':'B5vTA+wSQ0/S3w+rbawrLgVxGIdztJG1keqskxowzSHEDpWdpFdMHs69UgO4ei3R9aNDcqmjgANp49s/VhXiqw==',
-#         "LAWD_CD": "11110",
-#         "DEAL_YMD": "201512" }
-# encoded_args = urlencode(data)
-# res, content = http.request(url+encoded_args, method='GET')
-# print(res)
-# print(content)
-
 import urllib3
 from urllib.parse import urlencode, unquote, quote_plus
 
-url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?"
-data = {'serviceKey':'B5vTA+wSQ0/S3w+rbawrLgVxGIdztJG1keqskxowzSHEDpWdpFdMHs69UgO4ei3R9aNDcqmjgANp49s/VhXiqw==',
-        "LAWD_CD": "11110",
-        "DEAL_YMD": "201512" }
-encoded_args = urlencode(data)
+def make_restfull_query(lawd_cd, deal_ymd) :
+        url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?"
+        data = {
+                'serviceKey': 'B5vTA+wSQ0/S3w+rbawrLgVxGIdztJG1keqskxowzSHEDpWdpFdMHs69UgO4ei3R9aNDcqmjgANp49s/VhXiqw=='
+        }
+        data['LAWD_CD'] = lawd_cd
+        data['DEAL_YMD'] = deal_ymd
 
-http = urllib3.PoolManager()
-r = http.request('GET', url+encoded_args)
-print(r.status)
-print(r.data)
+        encoded_args = urlencode(data)
+
+        http = urllib3.PoolManager()
+        ret = http.request('GET', url + encoded_args)
+
+        return ret
+
+def print_xml(xml_str) :
+        import xml.etree.ElementTree as ET
+        apt_transactions = []
+
+        xml_s = xml_str.decode('utf8')
+        print(xml_s)
+        root = ET.fromstring(xml_s)
+
+        for item in root.iter('item') :
+                transaction = dict()
+                transaction['거래금액'] = item.find('거래금액').text
+                transaction['건축년도'] = item.find('건축년도').text
+                transaction['년'] = item.find('년').text
+                transaction['법정동'] = item.find('법정동').text
+                transaction['아파트'] = item.find('아파트').text
+                transaction['월'] = item.find('월').text
+                transaction['일'] = item.find('일').text
+                transaction['전용면적'] = item.find('전용면적').text
+                transaction['지번'] = item.find('지번').text
+                transaction['지역코드'] = item.find('지역코드').text
+                transaction['층'] = item.find('층').text
+                print(transaction)
+                apt_transactions.append(transaction)
 
 
+
+        
+def main():
+        result = make_restfull_query('41287', '201809')
+        print(result.status)
+        # print(result.data)
+        print_xml(result.data)
+        
+
+
+if __name__ == '__main__' :
+        main()
